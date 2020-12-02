0a43c286f339c8df43f373edf47dd961df3e19f1
Conor Shore <conorshore@gmail.com>
2020-12-02 16:52:16 +0000

    init

--- aSDIP.py
+++ aSDIP.py
@@ -7,13 +7,25 @@ import sys
 #this is the file you execute
 
 from src.aSDIP_UI import *
+from gpiozero import Button
+
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--interactive", help="Use in interactive mode",action="store_true")
 parser.add_argument("-s","--sniff",type=str,help="sets sniff interface for non interactive mode")
 parser.add_argument("-o","--output",type=str,help="sets output interface for non interactive mode")
+parser.add_argument("-b","--button", help="Use this to execute from button press on GPIO2",action="store_true")
 args = parser.parse_args()
+
+while true:
+    if(args.button):
+        button=Button(2)
+        if button.is_pressed:
+            print("pressed")
+        else:
+            print("no")
+
 
 if(args.interactive):
     try:
@@ -25,7 +37,7 @@ if(args.interactive):
         print()
         print("bye!")
 else:
-    log = open("aSDIP.log", "a")
+    log = open("/home/pi/aSDIP/aSDIP.log", "a")
     sys.stdout = log
     if(args.sniff is not None):
         main_ui().do_set_sniff_if(args.sniff)
